1e75048bdf4e032ab7e2cb1f9037185629c15c44
Daniel Pass <daniel.antony.pass@googlemail.com>
2026-02-08 23:03:57 +0000

    updates

--- Supplementary/P34B-simple-script.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-x = ["Monday", "Tuesday", "Wednesday", "Thursday"]
-y = ["a bit", "some", "loads", "I'm a genius"]
-
-plt.plot(x, y)
-plt.xlabel('Day of the course')
-plt.ylabel('How much python you know')
-plt.show()
